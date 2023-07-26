e84b8f6e276e03d8feab493e0ee77359c560d6d7
SALIK AHMED <92015547+ahmedsalik786@users.noreply.github.com>
2023-07-26 05:23:45 +0000

    Initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import { useState } from "react";
 
 const App = () => {
+  let [name,setName]=useState();
+  function fun(e){
+    setName(e.target.value)
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Enter your name:</p>
+        <input type="text" onChange={fun} name="" id="" />
+        {
+          name && (<p>Hello {name}!</p>)
+        }
+        
     </div>
   )
 }
